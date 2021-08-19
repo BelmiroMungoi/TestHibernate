5965e1f7254c754d4872e9e0a73de53b77582d0d
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-08-19 17:42:44 +0200

    adicionando o metodo de update e delete na classe User

--- src/main/java/com/bbm/TestHibernate/model/dao/UserDaoGeneric.java
+++ src/main/java/com/bbm/TestHibernate/model/dao/UserDaoGeneric.java
@@ -9,6 +9,7 @@ public class UserDaoGeneric<E> {
 
 	private EntityManager entityManager = HibernateUtil.getEntityManager();
 
+	// Metodo para a insercao de dados na base de dados
 	public void save(E entity) {
 		EntityTransaction transaction = entityManager.getTransaction();
 		transaction.begin();
@@ -16,9 +17,38 @@ public class UserDaoGeneric<E> {
 		transaction.commit();
 	}
 
+	// Metodo para a busca de dados na base de dados
 	public E search(E entity) {
 		Object id = HibernateUtil.getPrimaryKey(entity);
 		E e = (E) entityManager.find(entity.getClass(), id);
 		return e;
+	}
+
+	// Metodo para a busca de dados a serem atualizados
+	public E search(Long id, Class<E> entity) {
+		E e = (E) entityManager.find(entity, id);
+		return e;
+	}
+
+	// Metodo para a atualizacao de dados
+	public E updateMerge(E entity) {
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		E savedEntity = entityManager.merge(entity);
+		transaction.commit();
+
+		return savedEntity;
+	}
+
+	// Metodo para deletar os dados da base de dados
+	public void deleteById(E entity) {
+		Object id = HibernateUtil.getPrimaryKey(entity);
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		entityManager
+				.createNativeQuery(
+						"delete from " + entity.getClass().getSimpleName().toLowerCase() + " where idUser =" + id)
+				.executeUpdate();
+		transaction.commit();
 	}
 }
